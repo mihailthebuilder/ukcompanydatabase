c5e5a319b20948c2f9587f6d3de81a7e9175f891
mmarian <mihailmarian12@gmail.com>
2024-03-16 12:06:17 +0000

    contact form styling

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -9,14 +9,41 @@ function ContactForm() {
       <label htmlFor="email" className="hidden">
         Email Address
       </label>
-      <input id="email" type="email" name="email" />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <input
+        id="email"
+        type="email"
+        name="email"
+        className="w-full p-3 mt-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+        placeholder="Email"
+      />
+      <ValidationError
+        prefix="Email"
+        field="email"
+        errors={state.errors}
+        className="text-red-500"
+      />
       <label htmlFor="message" className="hidden">
         Message
       </label>
-      <textarea id="message" name="message" />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <button type="submit" disabled={state.submitting}>
+      <textarea
+        id="message"
+        name="message"
+        className="w-full p-3 mt-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+        placeholder="Your message..."
+      />
+      <ValidationError
+        prefix="Message"
+        field="message"
+        errors={state.errors}
+        className="text-red-500"
+      />
+      <button
+        type="submit"
+        disabled={state.submitting}
+        className={`w-full mt-6 ${
+          state.submitting ? "bg-gray-800" : "bg-indigo-600 hover:bg-indigo-700"
+        }   text-white font-bold py-2 px-4 rounded`}
+      >
         Submit
       </button>
     </form>
